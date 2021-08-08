e2628dd8ba359d2f937f4a1a556bb8fb1358f5cd
Darshan Vachhani <darshanvachhani08@gmail.com>
2021-08-08 13:45:23 +0530

    a

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flash_chat/screens/chat_screen.dart';
 import 'package:flash_chat/widgets/show_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 import 'package:flash_chat/widgets/rounded_button.dart';
 
 import '../constants.dart';
@@ -16,75 +17,92 @@ class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
   String? email;
   String? password;
+  bool isProcessing = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Hero(
-              tag: 'logo',
-              child: Container(
-                height: 200.0,
-                child: Image.asset('images/logo.png'),
+      body: ModalProgressHUD(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Hero(
+                tag: 'logo',
+                child: Container(
+                  height: 200.0,
+                  child: Image.asset('images/logo.png'),
+                ),
               ),
-            ),
-            SizedBox(
-              height: 48.0,
-            ),
-            TextField(
-              autofocus: true,
-              keyboardType: TextInputType.emailAddress,
-              textAlign: TextAlign.center,
-              style: TextStyle(color: Colors.black),
-              onChanged: (value) {
-                //Do something with the user input.
-                email = value;
-              },
-              decoration:
-                  kTextFieldDecoration.copyWith(hintText: 'Enter your Email'),
-            ),
-            SizedBox(
-              height: 8.0,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              obscureText: true,
-              onChanged: (value) {
-                //Do something with the user input.
-                password = value;
-              },
-              style: TextStyle(color: Colors.black),
-              decoration: kTextFieldDecoration.copyWith(
-                  hintText: 'Enter your Password'),
-            ),
-            SizedBox(
-              height: 24.0,
-            ),
-            RoundedButton(
-              title: 'Login',
-              onPress: () async {
-                try {
-                  final newUser = await _auth.signInWithEmailAndPassword(
-                      email: email!, password: password!);
-                  if (newUser.user != null) {
-                    Navigator.pushNamed(context, ChatScreen.id);
-                  } else {
+              SizedBox(
+                height: 48.0,
+              ),
+              TextField(
+                autofocus: true,
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.black),
+                onChanged: (value) {
+                  //Do something with the user input.
+                  email = value;
+                },
+                decoration:
+                    kTextFieldDecoration.copyWith(hintText: 'Enter your Email'),
+              ),
+              SizedBox(
+                height: 8.0,
+              ),
+              TextField(
+                textAlign: TextAlign.center,
+                obscureText: true,
+                onChanged: (value) {
+                  //Do something with the user input.
+                  password = value;
+                },
+                style: TextStyle(color: Colors.black),
+                decoration: kTextFieldDecoration.copyWith(
+                    hintText: 'Enter your Password'),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              RoundedButton(
+                title: 'Login',
+                onPress: () async {
+                  try {
+                    setState(() {
+                      isProcessing = true;
+                    });
+                    final userLogin = await _auth.signInWithEmailAndPassword(
+                        email: email!, password: password!);
+                    if (userLogin.user != null) {
+                      Navigator.pushNamed(context, ChatScreen.id);
+                    } else {
+                      setState(() {
+                        isProcessing = false;
+                      });
+                      showAlertDialog(
+                          context, "Email or Password are invalid!");
+                    }
+                    setState(() {
+                      isProcessing = false;
+                    });
+                  } catch (e) {
+                    setState(() {
+                      isProcessing = false;
+                    });
+                    print("Error =======$e");
                     showAlertDialog(context, "Email or Password are invalid!");
                   }
-                } catch (e) {
-                  print("Error =======$e");
-                  showAlertDialog(context, "Email or Password are invalid!");
-                }
-              },
-              color: Colors.lightBlueAccent,
-            ),
-          ],
+                },
+                color: Colors.lightBlueAccent,
+              ),
+            ],
+          ),
         ),
+        inAsyncCall: isProcessing,
       ),
     );
   }
