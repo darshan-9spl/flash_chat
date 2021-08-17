ad36fdd6bb2b312b85bec9a70058694ce17c51b9
desktop-6 <darshan.vachhani@9spl.com>
2021-08-17 16:43:04 +0530

    B

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+final firestore = FirebaseFirestore.instance;
+
 class ChatScreen extends StatefulWidget {
   static String id = 'chat_screen';
   @override
@@ -12,7 +14,7 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _auth = FirebaseAuth.instance;
-  final _firestore = FirebaseFirestore.instance;
+
   User? loggedInUser;
 
   TextEditingController _editMsg = TextEditingController();
@@ -28,6 +30,7 @@ class _ChatScreenState extends State<ChatScreen> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async {
+        await _auth.signOut();
         Navigator.popUntil(context, ModalRoute.withName(WelcomeScreen.id));
         return true;
       },
@@ -60,41 +63,7 @@ class _ChatScreenState extends State<ChatScreen> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: <Widget>[
-                StreamBuilder<QuerySnapshot>(
-                  stream: _firestore.collection('messages').snapshots(),
-                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
-                    if (!snapshot.hasData) {
-                      return Center(
-                        child: CircularProgressIndicator(
-                          backgroundColor: Colors.lightBlueAccent,
-                        ),
-                      );
-                    } else {
-                      final messages = snapshot.data!.docs;
-                      List<MessageBubble> messageWidgets = [];
-                      for (var message in messages) {
-                        Map msg = message.data() as Map<String, dynamic>;
-                        final messageTxt = msg['text'].toString();
-                        final messageSender = msg['sender'].toString();
-
-                        /*final messageWidget = Text(
-                          '$messageTxt from $messageSender',
-                          style: TextStyle(fontSize: 50),
-                        );*/
-                        final messageWidget = MessageBubble(
-                            text: messageTxt, sender: messageSender);
-                        messageWidgets.add(messageWidget);
-                      }
-                      return Expanded(
-                        child: ListView(
-                          padding: EdgeInsets.symmetric(
-                              horizontal: 10.0, vertical: 20.0),
-                          children: messageWidgets,
-                        ),
-                      );
-                    }
-                  },
-                ),
+                MessageStream(),
                 Container(
                   decoration: kMessageContainerDecoration,
                   child: Row(
@@ -110,7 +79,7 @@ class _ChatScreenState extends State<ChatScreen> {
                         onPressed: () {
                           //Implement send functionality.
                           if (_editMsg.text.isNotEmpty) {
-                            _firestore.collection('messages').add({
+                            firestore.collection('messages').add({
                               'text': _editMsg.text,
                               'sender': loggedInUser!.email
                             });
@@ -146,7 +115,7 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   void getMessage() async {
-    final messages = await _firestore.collection('messages').get();
+    final messages = await firestore.collection('messages').get();
     try {
       for (var message in messages.docs) {
         print("message=====" + message.data().toString());
@@ -157,11 +126,53 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 
   void messagesStreams() async {
-    await for (var snapshot in _firestore.collection('messages').snapshots()) {
+    await for (var snapshot in firestore.collection('messages').snapshots()) {
       for (var message in snapshot.docs) {
         print("message=====${message.data}");
       }
     }
+  }
+}
+
+class MessageStream extends StatelessWidget {
+  const MessageStream({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: firestore.collection('messages').snapshots(),
+      builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        } else {
+          final messages = snapshot.data!.docs;
+          List<MessageBubble> messageBubbles = [];
+          for (var message in messages) {
+            Map msg = message.data() as Map<String, dynamic>;
+            final messageTxt = msg['text'].toString();
+            final messageSender = msg['sender'].toString();
+
+            /*final messageWidget = Text(
+                          '$messageTxt from $messageSender',
+                          style: TextStyle(fontSize: 50),
+                        );*/
+            final messageBubble =
+                MessageBubble(text: messageTxt, sender: messageSender);
+            messageBubbles.add(messageBubble);
+          }
+          return Expanded(
+            child: ListView(
+              padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+              children: messageBubbles,
+            ),
+          );
+        }
+      },
+    );
   }
 }
 
@@ -172,11 +183,28 @@ class MessageBubble extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.lightBlueAccent,
-      child: Text(
-        '$text from $sender',
-        style: TextStyle(fontSize: 50),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        children: [
+          Text(
+            sender,
+            style: TextStyle(fontSize: 12.0, color: Colors.black54),
+          ),
+          Material(
+            color: Colors.lightBlueAccent,
+            elevation: 5.0,
+            borderRadius: BorderRadius.circular(30.0),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              child: Text(
+                '$text',
+                style: TextStyle(fontSize: 15, color: Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -35,11 +35,13 @@ class _LoginScreenState extends State<LoginScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: <Widget>[
-                Hero(
-                  tag: 'logo',
-                  child: Container(
-                    height: 200.0,
-                    child: Image.asset('images/logo.png'),
+                Flexible(
+                  child: Hero(
+                    tag: 'logo',
+                    child: Container(
+                      height: 200.0,
+                      child: Image.asset('images/logo.png'),
+                    ),
                   ),
                 ),
                 SizedBox(
@@ -91,8 +93,6 @@ class _LoginScreenState extends State<LoginScreen> {
                         setState(() {
                           isProcessing = false;
                         });
-                        showAlertDialog(
-                            context, "Email or Password are invalid!");
                       }
                       setState(() {
                         isProcessing = false;
